d41186524ad2958875c7b1609636d09d155546bc
mkendall11 <mattkendall321@gmail.com>
2024-02-17 20:15:13 +0000

    Rn output for delft file

--- Fabrication/Delft.py
+++ Fabrication/Delft.py
@@ -5,13 +5,19 @@ import pandas as pd
 df = pd.read_csv('Delft_Manhattan_raw_data.csv')
 
 df.dropna(subset=['Conductance (2W) [uS]'], inplace=True)
-df = df.loc[df['Conductance (2W) [uS]'] < 1e+20]
-print(df)
+df = df.loc[df['Conductance (2W) [uS]'] < 1e+10 ]
+df = df.loc[df['Conductance (2W) [uS]'] > 10 ]
+
 width = df['JJ Width (top electrode)'].tolist()
-conductance = df['Conductance (2W) [uS]'].tolist()
-print(conductance)
-print(width)
-plt.plot(width, conductance, '+')
+conductance = df['Conductance (2W) [uS]'].to_numpy()*10**-6
+x = df['X-Coordinate'].to_numpy()
+y = df['Y-Coordinate'].to_numpy()
+r = np.sqrt(x*x+y*y)
+Rn = 1/conductance
+
+plt.plot(width, Rn, '+')
 plt.xlabel('JJ width top electrode')
-plt.ylabel('Conductance (2W) [uS]')
+plt.ylabel(fr'$R_n$ $\Omega$')
 plt.show()
+
+
