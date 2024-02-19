4256f27e52655a317dd422f5016ecd27fbede660
mkendall11 <mattkendall321@gmail.com>
2024-02-19 13:35:36 +0000

    ML regression model

--- Fabrication/Delft.py
+++ Fabrication/Delft.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
 df = pd.read_csv('Delft_Manhattan_raw_data.csv')
 
@@ -15,9 +18,35 @@ y = df['Y-Coordinate'].to_numpy()
 r = np.sqrt(x*x+y*y)
 Rn = 1/conductance
 
-plt.plot(width, Rn, '+')
-plt.xlabel('JJ width top electrode')
-plt.ylabel(fr'$R_n$ $\Omega$')
+# plt.plot(width, Rn, '+')
+# plt.xlabel('JJ width top electrode')
+# plt.ylabel(fr'$R_n$ $\Omega$')
+# plt.show()
+
+# Combine features
+features = np.column_stack((r, width))
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(features, Rn, test_size=0.2, random_state=42)
+
+# Create a linear regression model
+model = LinearRegression()
+
+# Train the model
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+predictions = model.predict(X_test)
+
+# Evaluate the model
+mse = mean_squared_error(y_test, predictions)
+print(f'Mean Squared Error: {mse}')
+
+# Plotting predictions against true values
+plt.scatter(y_test, predictions)
+plt.plot([0, max(y_test)], [0, max(y_test)], '--', color='red')
+plt.xlabel('True Values')
+plt.ylabel('Predictions')
 plt.show()
 
-
+#11190840.635383697
